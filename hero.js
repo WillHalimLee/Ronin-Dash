db9b9cfebb57352f741ce738fd3e2ea359aa4056
Halim Lee <leehalim10@gmail.com>
2024-02-18 04:07:04 -0800

    yes

--- hero.js
+++ hero.js
@@ -1,171 +1,186 @@
 class Hero {
+  constructor(game, x, y) {
+    Object.assign(this, { game, x, y });
 
-    constructor(game, x, y) {
-        Object.assign(this, { game, x, y });
+    this.game.hero = this;
+    this.spritesheet = ASSET_MANAGER.getAsset("./sprites/hero.png");
 
-        this.game.hero = this;
-        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/hero.png");
+    this.x = x;
+    this.y = y;
+    this.jumpTick = 0;
+    this.fallTick = 0;
 
-        this.x = 150;
-        this.y = 400;
-        this.jumpTick = 0;
-        this.fallTick = 0;
+    this.dir = 0; // 0 = right, 1 = left
+    this.state = 0; // 0 = idle, 1 = parry, 2 = running, 3 = jumping, 4 = falling, 5 = attacking
 
-        this.dir = 0; // 0 = right, 1 = left
-        this.state = 0; // 0 = idle, 1 = parry, 2 = running, 3 = jumping, 4 = falling, 5 = attacking
+    this.health = 100;
+    this.baseDamage = 50;
 
-        this.health = 100;
-        this.baseDamage = 50;
+    this.animations = [];
+    this.loadAnimations();
+    this.updateBox();
+  }
 
-        this.animations = [];
-        this.loadAnimations();
+  loadAnimations() {
+    const HERO_WIDTH = 50;
+    const HERO_HEIGHT = 29;
 
-        this.updateBox();
-    };
-
-    loadAnimations() {
-
-        const HERO_WIDTH = 50;
-        const HERO_HEIGHT = 29;
-
-        for (let i = 0; i < 2; i++) {
-            this.animations.push([]);
-            for (let j = 0; j < 6; j++) {
-                this.animations[i].push([]);
-            }
-        }
-
-        // idle frames
-        this.animations[0][0] = new animator(this.spritesheet, 0, 0, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.08, true);
-
-        this.animations[1][0] = new animator(this.spritesheet, 0, HERO_HEIGHT, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.08, true);
-
-        // parry frames
-        this.animations[0][1] = new animator(this.spritesheet, HERO_WIDTH, 0, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.07, true);
-
-        this.animations[1][1] = new animator(this.spritesheet, HERO_WIDTH, HERO_HEIGHT,
-            HERO_WIDTH, HERO_HEIGHT, 1, 0.07, true);
-
-        // running frames
-        this.animations[0][2] = new animator(this.spritesheet, 2 * HERO_WIDTH, 0, HERO_WIDTH,
-            HERO_HEIGHT, 6, 0.08, true);
-
-        this.animations[1][2] = new animator(this.spritesheet, 2 * HERO_WIDTH, HERO_HEIGHT,
-            HERO_WIDTH, HERO_HEIGHT, 6, 0.08, true);
-
-        // jumping frames
-        this.animations[0][3] = new animator(this.spritesheet, 8 * HERO_WIDTH, 0, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.08, true);
-
-        this.animations[1][3] = new animator(this.spritesheet, 8 * HERO_WIDTH, HERO_HEIGHT, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.08, true);
-
-        // falling frames
-        this.animations[0][4] = new animator(this.spritesheet, 9 * HERO_WIDTH, 0, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.08, true);
-
-        this.animations[1][4] = new animator(this.spritesheet, 9 * HERO_WIDTH, HERO_HEIGHT, HERO_WIDTH,
-            HERO_HEIGHT, 1, 0.08, true);
-
-        // attacking frames
-        this.animations[0][5] = new animator(this.spritesheet, 10 * HERO_WIDTH, 0, HERO_WIDTH,
-            HERO_HEIGHT, 4, 0.06, true);
-
-        this.animations[1][5] = new animator(this.spritesheet, 10 * HERO_WIDTH, HERO_HEIGHT,
-            HERO_WIDTH, HERO_HEIGHT, 4, 0.06, true);
-    };
-
-    updateBox() {
-        this.box = new boundingbox(this.x + (15 * PARAMS.SCALE) - this.game.camera.x,
-            this.y + (6 * PARAMS.SCALE),
-            19 * PARAMS.SCALE,
-            22 * PARAMS.SCALE);
-    };
-
-    update() {
-
-        let that = this;
-
-        const MAX_FALL_VELOC = 7;
-
-        let canMoveLeft = true;
-        let canMoveRight = true;
-
-        // state defaults to falling
-        if (this.state != 3) {
-            this.state = 4;
-        }
-
-        // collision handling
-        this.game.stageTiles.forEach(function (tile) {
-            if (that.box.collide(tile.box)) {
-                if (that.box.bottom - tile.box.top <= 4 * PARAMS.SCALE) {
-                    if (that.dir == 0 && that.box.right >= tile.box.left + 3 * PARAMS.SCALE ||
-                        that.dir == 1 && that.box.left <= tile.box.right - 3 * PARAMS.SCALE) {
-                        that.jumpTick = 0;
-                        that.fallTick = 0;
-                        that.state = 0;
-                    }
-                } else if (that.dir == 0 && that.box.right > tile.box.left) {
-                    canMoveRight = false;
-                } else if (that.dir == 1 && that.box.left < tile.box.right) {
-                    canMoveLeft = false;
-                }
-
-                if (that.state == 3 && that.box.top - tile.box.bottom <= 4 * PARAMS.SCALE) {
-                    if (that.dir == 0 && that.box.right >= tile.box.left + 3 * PARAMS.SCALE ||
-                        that.dir == 1 && that.box.left <= tile.box.right - 3 * PARAMS.SCALE) {
-                        that.jumpTick = 0;
-                        that.fallTick = 3;
-                        that.state = 4;
-                    }
-                }
-            }
-        });
-
-        // jump
-        if (this.game.w && this.state != 3 && this.state != 4) {
-            this.state = 3;
-            this.deltaY = -24;
-        }
-
-        // y updates for jumping
-        if (this.state == 3) {
-            if (this.jumpTick < 35) {
-                this.jumpTick++;
-                this.y -= 7 - (0.2 * this.jumpTick);
-            } else {
-                this.jumpTick = 0;
-                this.fallTick = 0;
-                this.state = 4;
-            }
-        } else if (this.state == 4) {
-            this.fallTick++;
-            this.y += 0.2 * this.fallTick <= MAX_FALL_VELOC ? 0.2 * this.fallTick : MAX_FALL_VELOC;
-        }
-
-        // updates for left/right movement
-        if (this.game.d && !this.game.a) {
-            if (this.state != 3 && this.state != 4) this.state = 2;
-            this.dir = 0;
-            if (canMoveRight) this.x += 5;
-        } else if (this.game.a && !this.game.d) {
-            if (this.state != 3 && this.state != 4) this.state = 2;
-            this.dir = 1;
-            if (canMoveLeft) this.x -= 5;
-        }
-
-        console.log(this.state);
-        this.updateBox();
+    for (let i = 0; i < 2; i++) {
+      this.animations.push([]);
+      for (let j = 0; j < 6; j++) {
+        this.animations[i].push([]);
+      }
     }
 
-    draw(ctx) {
-        this.animations[this.dir][this.state].drawFrame(this.game.clockTick, ctx,
-            this.x - this.game.camera.x, this.y,
-            PARAMS.SCALE);
-    };
+    // idle frames
+    this.animations[0][0] = new animator(this.spritesheet, 0, 0, HERO_WIDTH, HERO_HEIGHT, 1, 0.08, true);
+    this.animations[1][0] = new animator(this.spritesheet, 0, HERO_HEIGHT, HERO_WIDTH, HERO_HEIGHT, 1, 0.08, true);
 
+    // running frames
+    this.animations[0][2] = new animator(this.spritesheet, 2 * HERO_WIDTH, 0, HERO_WIDTH, HERO_HEIGHT, 6, 0.08, true);
+    this.animations[1][2] = new animator(
+      this.spritesheet,
+      2 * HERO_WIDTH,
+      HERO_HEIGHT,
+      HERO_WIDTH,
+      HERO_HEIGHT,
+      6,
+      0.08,
+      true
+    );
+
+    // jumping frames
+    this.animations[0][3] = new animator(this.spritesheet, 8 * HERO_WIDTH, 0, HERO_WIDTH, HERO_HEIGHT, 1, 0.08, true);
+    this.animations[1][3] = new animator(
+      this.spritesheet,
+      8 * HERO_WIDTH,
+      HERO_HEIGHT,
+      HERO_WIDTH,
+      HERO_HEIGHT,
+      1,
+      0.08,
+      true
+    );
+
+    // falling frames
+    this.animations[0][4] = new animator(this.spritesheet, 9 * HERO_WIDTH, 0, HERO_WIDTH, HERO_HEIGHT, 1, 0.08, true);
+
+    this.animations[1][4] = new animator(
+      this.spritesheet,
+      9 * HERO_WIDTH,
+      HERO_HEIGHT,
+      HERO_WIDTH,
+      HERO_HEIGHT,
+      1,
+      0.08,
+      true
+    );
+  }
+
+  updateBox() {
+    this.box = new boundingbox(
+      this.x + 15 * PARAMS.SCALE - this.game.camera.x,
+      this.y + 8 * PARAMS.SCALE,
+      19 * PARAMS.SCALE,
+      22 * PARAMS.SCALE
+    );
+  }
+
+  update() {
+    console.log(this.x);
+    let that = this;
+
+    const MAX_FALL_VELOC = 6;
+    const MIN_X_BOUNDARY = 0;
+
+    let canMoveLeft = true;
+    let canMoveRight = true;
+
+    // state defaults to falling
+    if (this.state != 3) {
+      this.state = 4;
+    }
+
+    // collision handling
+    this.game.stageTiles.forEach(function (tile) {
+      if (that.box.collide(tile.box)) {
+        if (that.box.bottom - tile.box.top <= 4 * PARAMS.SCALE) {
+          if (
+            (that.dir == 0 && that.box.right >= tile.box.left + 4 * PARAMS.SCALE) ||
+            (that.dir == 1 && that.box.left <= tile.box.right - 4 * PARAMS.SCALE)
+          ) {
+            that.jumpTick = 0;
+            that.fallTick = 0;
+            that.state = 0;
+          }
+        } else if (that.dir == 0 && that.box.right > tile.box.left) {
+          canMoveRight = false;
+          this.x = this.x - 10;
+        } else if (that.dir == 1 && that.box.left < tile.box.right) {
+          canMoveLeft = false;
+        }
+
+        if (that.state == 3 && that.box.top - tile.box.bottom <= 4 * PARAMS.SCALE) {
+          if (
+            (that.dir == 0 && that.box.right >= tile.box.left + 4 * PARAMS.SCALE) ||
+            (that.dir == 1 && that.box.left <= tile.box.right - 4 * PARAMS.SCALE)
+          ) {
+            that.jumpTick = 0;
+            that.fallTick = 3;
+            that.state = 4;
+          }
+        }
+      }
+    });
+
+    // jump
+    if (this.game.w && this.state != 3 && this.state != 4) {
+      this.state = 3;
+      this.deltaY = -24;
+    }
+    // y updates for jumping
+    if (this.state == 3) {
+      if (this.jumpTick < 35) {
+        this.jumpTick++;
+        this.y -= 8 - 0.2 * this.jumpTick;
+      } else {
+        this.jumpTick = 0;
+        this.fallTick = 0;
+        this.state = 4;
+      }
+    } else if (this.state == 4) {
+      this.fallTick++;
+      this.y += 0.2 * this.fallTick <= MAX_FALL_VELOC ? 0.2 * this.fallTick : MAX_FALL_VELOC;
+    }
+
+    if (this.game.d && !this.game.a) {
+      if (this.state != 3 && this.state != 4) this.state = 2;
+      this.dir = 0;
+      if (canMoveRight) this.x += 1;
+    } else if (this.game.a && !this.game.d) {
+      if (this.state != 3 && this.state != 4) this.state = 2;
+      this.dir = 1;
+      if (canMoveLeft && this.x > MIN_X_BOUNDARY) this.x -= 1; // Check against MIN_X_BOUNDARY
+    }
+    this.x = this.x + 4;
+    this.updateBox();
+  }
+
+  draw(ctx) {
+    this.animations[this.dir][this.state].drawFrame(
+      this.game.clockTick,
+      ctx,
+      this.x - this.game.camera.x,
+      this.y,
+      PARAMS.SCALE
+    );
+    // Draw the bounding box for debugging
+    ctx.strokeStyle = "red"; // Set the color of the bounding box
+    ctx.strokeRect(
+      this.box.x, // X position of the bounding box
+      this.box.y, // Y position of the bounding box
+      this.box.width, // Width of the bounding box
+      this.box.height // Height of the bounding box
+    );
+  }
 }
--- scenemanager.js
+++ scenemanager.js
@@ -1,90 +1,77 @@
 class SceneManager {
-    constructor(game) {
-        this.game = game;
-        this.game.camera = this;
-        this.x = 0;
-        this.hero = new Hero(this.game, 150, 490);
-        // this.wolf = new Wolf(this.game, -20, 490);
-        this.load(levelOne);
-    };
+  constructor(game) {
+    this.game = game;
+    this.game.camera = this;
 
-    clearEntities() {
-        this.game.entities.forEach(function (entity) {
-            entity.removeFromWorld = true;
-        });
-    };
+    this.hero = new Hero(this.game, 150, 0);
+    this.game.addEntity(new Background(this.game));
+    this.game.addEntity(this.hero);
+    this.floorTiles = [];
+    this.lastTileX = 512;
 
-    /* This method loads everything we see on the canvas.
-     * @Params level : takes the games level, which loads the coresponding json object.
-     */
-    load(level) {
-        this.level = level;
-        this.game.addEntity(new Background(this.game));
+    this.loadFirst();
+  }
 
-        for (let i = 0; i < level.floor1.length; i++) {
-            let floorOne = level.floor1[i];
-            let tile = new Floor1(this.game, floorOne.x, floorOne.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.floor2.length; i++) {
-            let floorTwo = level.floor2[i];
-            let tile = new Floor2(this.game, floorTwo.x, floorTwo.y)
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.floor3.length; i++) {
-            let floorThree = level.floor3[i];
-            let tile = new Floor3(this.game, floorThree.x, floorThree.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.underfloor.length; i++) {
-            let underfloor = level.underfloor[i];
-            let tile = new Underfloor(this.game, underfloor.x, underfloor.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.lpillar.length; i++) {
-            let pLeft = level.lpillar[i];
-            let tile = new PillarL(this.game, pLeft.x, pLeft.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.rpillar.length; i++) {
-            let pRight = level.rpillar[i];
-            let tile = new PillarR(this.game, pRight.x, pRight.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.lplatform.length; i++) {
-            let platformL = level.lplatform[i];
-            let tile = new PlatformL(this.game, platformL.x, platformL.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        for (let i = 0; i < level.rplatform.length; i++) {
-            let platformR = level.rplatform[i];
-            let tile = new PlatformR(this.game, platformR.x, platformR.y);
-            this.game.addEntity(tile);
-            this.game.addStageTile(tile);
-        }
-        this.game.addEntity(this.hero);
-    };
+  clearEntities() {
+    this.game.entities.forEach(function (entity) {
+      entity.removeFromWorld = true;
+    });
+  }
+  /* This method loads everything we see on the canvas.
+   * @Params level : takes the games level, which loads the coresponding json object.
+   */
+  loadFirst() {
+    for (let i = 0; i < 3; i++) {
+      let tile = new Floor(this.game, 256 * i, 500);
+      this.game.addEntity(tile);
+      this.game.addStageTile(tile);
+    }
+  }
 
-    updateAudio() {
-        //todo
-    };
+  updateAudio() {
+    //todo
+  }
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  addNewFloorTiles() {
+    // Assuming each new set of tiles starts at the lastTileX + 256
+    let baseX = this.lastTileX + 256;
 
-    update() {
-        let xmidpoint = PARAMS.CANVAS_WIDTH / 2 - PARAMS.BLOCKWIDTH / 2;
-        let ymidpoint = PARAMS.CANVAS_HEIGHT / 2 - PARAMS.BLOCKWIDTH / 2;
-        this.x = this.hero.x - xmidpoint + PARAMS.BLOCKWIDTH / 2;
-        this.y = this.hero.y - ymidpoint + PARAMS.BLOCKWIDTH / 2 - PARAMS.BLOCKWIDTH;
+    // Remove the oldest four tiles if there are enough tiles
+    while (this.floorTiles.length > 4) {
+      let oldTile = this.floorTiles.shift(); // Remove from the beginning of the array
+      oldTile.removeFromWorld = true; // Mark it for removal from the game
+    }
 
-    };
+    // Add four new tiles
+    for (let i = 0; i < 4; i++) {
+      let randomInt = this.getRandomIntInclusive(0, 3); // Generates a random integer between 0 and 3
+      let tileX = baseX + 256 * i;
+      let tile = new Floor(this.game, tileX, 400 + randomInt * 100);
+      this.game.addEntity(tile);
+      this.game.addStageTile(tile);
+      this.floorTiles.push(tile); // Keep track of this tile
+    }
 
-    draw(ctx) {
+    // Update the lastTileX to reflect the position of the new last tile
+    this.lastTileX = baseX + 256 * 3; // Position of the new last tile
+  }
 
-    };
-};
+  // Example usage:
+
+  update() {
+    let xmidpoint = PARAMS.CANVAS_WIDTH / 2 - PARAMS.BLOCKWIDTH / 2;
+    let ymidpoint = PARAMS.CANVAS_HEIGHT / 2 - PARAMS.BLOCKWIDTH / 2;
+    this.x = this.hero.x - xmidpoint + PARAMS.BLOCKWIDTH / 2;
+    this.y = this.hero.y - ymidpoint + PARAMS.BLOCKWIDTH / 2 - PARAMS.BLOCKWIDTH;
+
+    if (this.hero.x >= this.lastTileX - 512) {
+      this.addNewFloorTiles();
+    }
+  }
+
+  draw(ctx) {}
+}
